8b775e21e92d5d90db913e491714dcc4c56f63bd
skt-t1-byungi <tiniwz@gmail.com>
2018-08-01 11:47:40 +0900

    simplify reduce, reduceRight

--- packages/reduceRight/reduceRight.test.js
+++ packages/reduceRight/reduceRight.test.js
@@ -4,14 +4,13 @@ import reduceRight from './reduceRight'
 test('reduceRight', t => {
   const stub1 = [1, 2, 3, 4]
   stub1.reduce = undefined
-  t.is(reduceRight(stub1, (acc, v) => acc + v), 10)
-  t.is(reduceRight(stub1, (acc, v) => acc + v, 5), 15)
+  t.is(reduceRight(stub1, (acc, v) => acc + v, 1), 11)
 
   // for seq
   t.is(reduceRight(stub1, (acc, v) => v), 1)
 
   const stub2 = [{x: 1}, {x: 2}, {x: 3}]
   stub2.reduceRight = undefined
-  t.deepEqual(reduceRight(stub2, (acc, o) => ({x: acc.x + o.x})), {x: 6})
+  t.deepEqual(reduceRight(stub2, (acc, o) => ({x: (acc.x || 0) + o.x}), {}), {x: 6})
   t.is(reduceRight(stub2, (acc, o) => acc + o.x, 5), 11)
 })
--- packages/reduceRight/reduceRight.ts
+++ packages/reduceRight/reduceRight.ts
@@ -1,18 +1,14 @@
 type Reducer<T,U> = (prev: U, value: T, index: number, array: T[]) => U
 
-function reduceRight<T> (arr: T[], reducer: Reducer<T,T>): T
-function reduceRight<T,U> (arr: T[], reducer: Reducer<T,U>, initValue ?: U) {
-  if (arr.reduceRight) {
-    if (arguments.length === 2) return arr.reduceRight(reducer as any)
-    return arr.reduceRight(reducer, initValue as U)
-  }
+function reduceRight<T,U> (arr: T[], reducer: Reducer<T,U>, initValue: U) {
+  if (arr.reduceRight) return arr.reduceRight(reducer, initValue as U)
 
   const len = arr.length
   let i = len
-  let res = arguments.length === 2 ? arr[--i] : initValue
+  let res = initValue
 
   while (i--) {
-    res = reducer(res as U, arr[i], i, arr)
+    res = reducer(res, arr[i], i, arr)
   }
 
   return res
